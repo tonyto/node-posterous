b06717f7f825ac26a441427fbf87038df63f5095
Andy <zegg90@gmail.com>
2011-06-11 13:41:14 +0200

    Basic api requests and api token request done, including test

---
+++ tests/auth.js
@@ -0,0 +1,33 @@
+var Posterous = require('../index');
+var config = require('./config');
+
+module.exports['API Token'] = function(test) {
+  //To test, add your posterous email/password
+  var posterous = new Posterous(config.user, config.pass);
+
+  posterous.auth(function(err, result) {
+    //Don't expect an error
+    test.ok(!err, 'An error occurred: ' + err);
+
+    //Expect a result
+    test.ok(result, 'No api token was returned.');
+    console.log('API token: ' + result);
+
+    test.done();
+  });
+};
+
+module.exports['Bad Auth'] = function(test) {
+  //Make a bad api token request with no username/password
+  var posterous = new Posterous('', '');
+
+  posterous.auth(function(err, result) {
+    //Expect an error
+    test.ok(err);
+
+    //Don't expect a result
+    test.ok(!result);
+
+    test.done();
+  });
+};
---
+++ tests/config.sample.js
@@ -0,0 +1,7 @@
+//To run the tests, rename this file to config.js
+//and fill in your posterous account details
+
+module.exports = {
+  user: 'your email',
+  pass: 'your password'
+};
